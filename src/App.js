67875577a19f89cfc8be7932e07e56ea3f5fd8e1
yongfire38 <yongfire38@gmail.com>
2023-01-30 14:34:02 +0900

    contribution 추가 수정 2

--- src/App.js
+++ src/App.js
@@ -23,5 +23,5 @@ console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 console.log("process.env.REACT_APP_EGOV_CONTEXT_URL", process.env.REACT_APP_EGOV_CONTEXT_URL);
 
 // contribution 연습용 주석, rebase 완료
-// contribution 추가 수정
+// contribution 추가 수정 2
 export default App;
