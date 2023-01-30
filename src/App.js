f45e913084c24bfd55f8488465da5c7690162cfb
yongfire38 <yongfire38@gmail.com>
2023-01-30 14:12:31 +0900

    conflict 수정

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ import './css/response.css';
 function App() {
 
   return (
-    // 테스트 주석
-    // main branch에서 수정
-    // main branch에만 반영시킬 코드
+
     <div className="wrap">
       <React.StrictMode>
         <RootRoutes />
@@ -24,5 +22,6 @@ function App() {
 console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 console.log("process.env.REACT_APP_EGOV_CONTEXT_URL", process.env.REACT_APP_EGOV_CONTEXT_URL);
 
-// contribution 에서 수정한 주석
+// rebase 연습용 주석
+// contribution 연습용 주석
 export default App;
