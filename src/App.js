d03d9303169c5745909ced12774c30d4e5952d51
yongfire38 <yongfire38@gmail.com>
2023-02-28 13:37:28 +0900

    서브 브랜치 내용을 변경

--- src/App.js
+++ src/App.js
@@ -1,202 +1,115 @@
-import logo from './logo.svg';
+import React, { useEffect, useReducer, useRef } from 'react';
+
 import './App.css';
-import {useState} from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-function Header(props) {
-  //console.log('props',props, props.title);
-  return (
-    <header>
-      <h1><a href='/' onClick={(event)=>{
-        event.preventDefault();
-        props.onChangeMode();
-      }}>{props.title}</a></h1>
-    </header>
-  );
-}
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
 
-function Nav(props) {
-  const lis = [];
-  
-  for(let i = 0; i < props.topics.length; i++){
-    let t = props.topics[i];
-    lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
-        event.preventDefault();
-        props.onChangeMode(Number(event.target.id));
-      }}>{t.title}</a>
-    </li>);
+const reducer = (state, action) => {
+  let newState = [];
+  switch(action.type) {
+    case 'INIT':{
+      return action.data;
+    }
+    case 'CREATE':{
+      const newItem = {...action.data}
+      newState = [newItem, ...state];
+      break;
+    }
+    case 'REMOVE': {
+      newState = state.filter((it)=>it.id !== action.targetId);
+      break;
+    }
+    case 'EDIT': {
+      newState = state.map((it) => 
+        it.id === action.data.id ? { ...action.data }: it
+      );
+      break;
+    }
+    default:
+      return state;
   }
 
-  return (
-    <nav>
-      <ol>
-        {lis}
-      </ol>
-    </nav>
-  );
-}
+  console.log(newState);
 
-function Article(props) {
-  //console.log('props',props, props.title, props.body);
-  return (
-    <article>
-      <h2>{props.title}</h2>
-      {props.body}
-    </article>
-  );
-}
+  localStorage.setItem('diary',JSON.stringify(newState));
 
-function Create(props) {
-  return (
-    <article>
-      <h2>Create</h2>
-      <form onSubmit={event=>{
-        event.preventDefault();
+  return newState;
 
-        const title = event.target.title.value;
-        const body = event.target.body.value;
 
-        props.onCreate(title, body);
-      }}>
-        <p><input type="text" name="title" placeholder='title'/></p>
-        <p><textarea name="body" placeholder='body'></textarea></p>
-        <p><input type="submit" value="Create" /></p>
-      </form>
-    </article>
-  );
-}
+};
 
-function Update(props) {
-  const [title, setTitle] = useState(props.title);
-  const [body, setBody] = useState(props.body);
-  return (
-    <article>
-      <h2>Update</h2>
-      <form onSubmit={event=>{
-        event.preventDefault();
-
-        const title = event.target.title.value;
-        const body = event.target.body.value;
-
-        props.onUpdate(title, body);
-      }}>
-        <p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{
-          setTitle(event.target.value);
-        }}/></p>
-        <p><textarea name="body" placeholder="body" value={body} onChange={event=>{
-          setBody(event.target.value);
-        }}></textarea></p>
-        <p><input type="submit" value="Update" /></p>
-      </form>
-    </article>
-  );
-}
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
 function App() {
-  const [mode, setMode] = useState('WELCOME');
-  const [id, setId] = useState(null);
-  const [nextId, setNextId] = useState(4);
 
-  const [topics, setTopics] = useState([
-    {id:1, title:'html', body:'html is...'},
-    {id:2, title:'css', body:'css is...'},
-    {id:3, title:'javascript', body:'javascript is...'}
-  ]);
-
-  let content =  null;
-  let contextControll = null;
-
-  if(mode === 'WELCOME'){
-    content = <Article title="Welcome" body="Hello, web"></Article>
-  } else if(mode === 'READ'){
-    let title, body = null;
-
-    for(let i = 0; i < topics.length; i++){
-      console.log(topics[i].id, id);
-      if(topics[i].id === id) {
-        title = topics[i].title;
-        body = topics[i].body;
+  useEffect(()=> {
+    const localData = localStorage.getItem('diary');
+    if(localData) {
+      const diaryList = JSON.parse(localData).sort((a,b) => parseInt(b.id) - parseInt(a.id));
+      
+      if(diaryList.length >= 1) {
+        dataId.current = parseInt(diaryList[0].id) + 1;
+        dispatch({type:"INIT",data : diaryList});
       }
 
     }
-    content = <Article title={title} body={body}></Article>
-    contextControll = <>
-      <li><a href={"/update/"+id} onClick={event=>{
-        event.preventDefault();
-        setMode('UPDATE');
-      }}>Update</a></li>
-      <li><input type="button" value="Delete" onClick={()=>{
-        const newTopics =[];
+  },[]);
 
-        for(let i = 0; i < topics.length; i++){
-          if(topics[i].id !== id){
-            newTopics.push(topics[i]);
-          }
-        }
+  const [data, dispatch] = useReducer(reducer,[]);
 
-        setTopics(newTopics);
-        setMode('WELCOME');
-      }}/></li></>
-  } else if(mode === 'CREATE'){
-    content = <Create onCreate={(_title, _body)=>{
-      const newTopic = {id:nextId, title:_title, body:_body}
-      const newTopics = [...topics]
-      newTopics.push(newTopic);
-      setTopics(newTopics);
-      setMode('READ');
-      setId(nextId);
-      setNextId(nextId+1);
-    }}>
-    </Create>
-  } else if(mode === 'UPDATE'){
-    let title, body = null;
+  const dataId = useRef(0);
 
-    for(let i = 0; i < topics.length; i++){
-      console.log(topics[i].id, id);
-      if(topics[i].id === id) {
-        title = topics[i].title;
-        body = topics[i].body;
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({type:"CREATE", data:{
+      id: dataId.current,
+      date: new Date(date).getTime(),
+      content,
+      emotion
+    }})
+    dataId.current += 1;
+  }
+
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({type:"REMOVE", targetId});
+  }
+
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type:"EDIT", 
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion
       }
-
-    }
-
-    content = <Update title={title} body={body} onUpdate={(title, body)=>{
-      console.log(title, body);
-      const newTopics = [...topics];
-      const updatedTopic = {id:id, title:title, body:body}
-
-      for(let i=0; i < newTopics.length; i++){
-        if(newTopics[i].id === id){
-          newTopics[i] = updatedTopic;
-          break;
-        }
-      }
-      setTopics(newTopics);
-      setMode('READ');
-    }}>
-    </Update>
+    });
   }
 
   return (
-    <div>
-      <Header title="WEB" onChangeMode={()=>{
-        setMode('WELCOME');
-      }}></Header>
-      <Nav topics={topics} onChangeMode={(_id)=>{
-        setMode('READ');
-        setId(_id);
-      }}></Nav>
-      {content}
-      <ul>
-        <li>
-          <a href='/create' onClick={event =>{
-            event.preventDefault();
-            setMode('CREATE');
-          }}>Create</a>
-        </li>
-        {contextControll}
-      </ul>
-    </div>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{
+         onCreate, onEdit, onRemove
+      }}>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<New />} />
+          <Route path='/edit/:id' element={<Edit />} />
+          <Route path='/diary/:id' element={<Diary />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
